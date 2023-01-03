dc475f295a7bc6a0e79b4128925cedab1be25274
RuslanGrinov <ruslan.hrinov@gmail.com>
2023-01-03 22:09:12 +0200

    commit

--- src/script/modules/game-process.js
+++ src/script/modules/game-process.js
@@ -1,6 +1,8 @@
 import itemsMoveIntoPosition from "./itemsMoveIntoPosition";
 const gameProcess = () => {
   const lastSwitchSound = new Audio('https://www.fesliyanstudios.com/play-mp3/5269');
+  lastSwitchSound.volume = 0.5;
+  console.dir(lastSwitchSound);
   const item = document.querySelectorAll('.item');
   const gameTable = document.querySelector('.game-table');
   const resultsTable = document.querySelector('.results-table');
